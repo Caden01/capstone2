697c6686e4299b2f3081eb306cf6c1c3661072b0
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2024-04-05 13:53:10 -0700

    Create js file to add API data into countries database

---
+++ backend/db.js
@@ -0,0 +1,77 @@
+const axios = require("axios");
+const { Client } = require("pg");
+
+async function getDataFromAPI(url) {
+  try {
+    const res = await axios.get(url);
+    return res.data;
+  } catch (err) {
+    console.error(`Error fetching data from ${url}`, err);
+    return null;
+  }
+}
+
+async function insertIntoDatabase(data, tableName) {
+  let db = new Client({
+    connectionString: "postgresql:///countries",
+  });
+
+  db.connect();
+
+  const tableQeury = `
+    CREATE TABLE IF NOT EXISTS ${tableName} (
+      id SERIAL PRIMARY KEY,
+      country_name VARCHAR(255),
+      capital VARCHAR(255),
+      region VARCHAR(255),
+      subregion VARCHAR(255),
+      population INT
+  );
+  `;
+
+  await db.query(tableQeury);
+
+  for (const item of data) {
+    const insertQuery = `
+      INSERT INTO ${tableName} (country_name, capital, region, subregion, population)
+      VALUES ($1, $2, $3, $4, $5)
+    `;
+
+    let values;
+
+    if (item.capital) {
+      values = [
+        item.name.common,
+        item.capital[0],
+        item.region,
+        item.subregion,
+        item.population,
+      ];
+    } else {
+      values = [
+        item.name.common,
+        null,
+        item.region,
+        item.subregion,
+        item.population,
+      ];
+    }
+
+    await db.query(insertQuery, values);
+  }
+
+  console.log("Data was succesfully entered");
+}
+
+async function main() {
+  const url = "https://restcountries.com/v3.1/all";
+  const tableName = "countries";
+
+  const data = await getDataFromAPI(url);
+
+  if (data) {
+    await insertIntoDatabase(data, tableName);
+  }
+}
+
+main();
