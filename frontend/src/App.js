2edca86265b6b39d42f286d636dc4869b1752e7c
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2024-04-05 14:59:14 -0700

    Set up navbar on react frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Nav from "./routes/Nav";
 
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Nav />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
