fe559ba6c9e0b73e49801eaa51fe58924410fe21
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2024-04-06 16:45:10 -0700

    Create model for country allowing to search for all countries or a specific country

---
+++ backend/models/countries.js
@@ -0,0 +1,51 @@
+const db = require("../db");
+const { NotFoundError } = require("../expressError");
+
+class Country {
+  /** Find all countries
+   *
+   * Returns [{ country_name, capital, region, subregion, population}]
+   */
+  static async findAll() {
+    const countryRes = await db.query(
+      `
+       SELECT country_name,
+              capital,
+              region,
+              subregion,
+              population
+       FROM countries
+       ORDER BY name
+      `,
+    );
+    return countryRes.rows;
+  }
+
+  /** Return data about a specific country
+   *
+   * Returns { country_name, capital, region, subregion, population }
+   *
+   * Throws NotFoundError if not found
+   */
+
+  static async get(country_name) {
+    const countryRes = await db.query(
+      `SELECT country_name,
+              capital,
+              region,
+              subregion,
+              population,
+       FROM countries
+       WHERE country_name = $1
+      `[country_name],
+    );
+
+    const country = countryRes.rows[0];
+
+    if (!country) throw new NotFoundError(`No country: ${country_name}`);
+
+    return country;
+  }
+}
+
+module.exports = Country;
